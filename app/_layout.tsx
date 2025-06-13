a0104301d956a2b3fe26d86318015d9c60626eaa
Mathias Klenk <mathias.j.klenk@gmail.com>
2025-06-13 10:59:02 +0200

    Refactor ClerkProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,12 @@
 import { useFonts } from "expo-font";
 import { Slot, SplashScreen, Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
-import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
-import * as SecureStore from "expo-secure-store";
+import { useAuth } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
-import { ActivityIndicator, TouchableOpacity, View } from "react-native";
+import { TouchableOpacity } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-// Cache the Clerk JWT
-const tokenCache = {
-  async getToken(key: string) {
-    try {
-      return SecureStore.getItemAsync(key);
-    } catch (err) {
-      return null;
-    }
-  },
-  async saveToken(key: string, value: string) {
-    try {
-      return SecureStore.setItemAsync(key, value);
-    } catch (err) {
-      return;
-    }
-  },
-};
+import { ClerkProvider } from "../providers/ClerkProvider";
+import { NavigationContainer } from "@react-navigation/native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -94,13 +76,12 @@ const InitialLayout = () => {
 
 const RootLayoutNav = () => {
   return (
-    <ClerkProvider
-      publishableKey={CLERK_PUBLISHABLE_KEY!}
-      tokenCache={tokenCache}
-    >
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <InitialLayout />
-      </GestureHandlerRootView>
+    <ClerkProvider>
+      <NavigationContainer>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <InitialLayout />
+        </GestureHandlerRootView>
+      </NavigationContainer>
     </ClerkProvider>
   );
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import AnimatedIntro from '@/components/AnimatedIntro';
-import BottomLoginSheet from '@/components/BottomLoginSheet';
-import { View, StyleSheet } from 'react-native';
+import AnimatedIntro from "@/components/AnimatedIntro";
+import BottomLoginSheet from "@/components/BottomLoginSheet";
+import { View, StyleSheet } from "react-native";
+
 const Page = () => {
   return (
     <View style={styles.container}>
